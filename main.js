531fd8641c3edea45e16a8b07cc92a7959f0db3c
anupupgirl <86975247+anupupgirl@users.noreply.github.com>
2022-01-09 10:21:52 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,76 @@
+prediction_1 = "";
+prediction_2 = "";
+
+Webcam.set({
+  width: 350,
+  height: 300,
+  image_format: "png",
+  png_quailty: 90,
+});
+
+camera = document.getElementById("camera");
+Webcam.attach("#camera");
+
+function take_snapshot() {
+  Webcam.snap(function (data_uri) {
+    document.getElementById("result").innerHTML =
+      '<img id="capture_image" src="' + data_uri + '">';
+  });
+}
+
+console.log("ml5 version", ml5.version);
+classifier = ml5.imageClassifier(
+  "https://teachablemachine.withgoogle.com/models/9gIkcCefW/model.json",
+  model_loded
+);
+function model_loded() {
+  console.log("model loaded!");
+}
+
+function speak() {
+  var synth = window.speechSynthesis;
+  speak_data_1 = "The first prediction is " + prediction_1;
+  speak_data_2 = "And the second prediction is " + prediction_2;
+  var utterthis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+  synth.speak(utterthis);
+}
+
+function check() {
+  img = document.getElementById("capture_image");
+  classifier.classify(img, gotresult);
+}
+function gotresult(error, result) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(result);
+    document.getElementById("result_emotion_name").innerHTML = result[0].label;
+    document.getElementById("result_emotion_name2").innerHTML = result[1].label;
+    prediction_1 = result[0].label;
+    prediction_2 = result[1].label;
+    speak();
+    if (result[0].label == "best") {
+      document.getElementById("update_emoji").innerHTML = "&#128077";
+    }
+
+    if (result[0].label == "amazing") {
+      document.getElementById("update_emoji").innerHTML = "&#128076";
+    }
+
+    if (result[0].label == "victory") {
+      document.getElementById("update_emoji").innerHTML = "&#9996";
+    }
+
+    if (result[1].label == "best") {
+      document.getElementById("update_emoji2").innerHTML = "&#128077";
+    }
+
+    if (result[1].label == "amazing") {
+      document.getElementById("update_emoji2").innerHTML = "&#128076";
+    }
+
+    if (result[1].label == "victory") {
+      document.getElementById("update_emoji2").innerHTML = "&#9996";
+    }
+  }
+}
